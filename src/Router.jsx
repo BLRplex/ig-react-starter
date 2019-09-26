067d7c8052a702707b2f21ed1badc952bc5dc3a8
Maksim Tarhunakau <maksimtarhunakau@MacBook-Pro-Maksim-2.local>
2019-09-26 23:01:55 +0300

    Adding 404 page

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Loader from '@/components/blocks/global/Loader'
 import SecuredRoute from '@/components/wrappers/SecuredRoute'
@@ -11,19 +11,29 @@ import {
 import { LANDING_PAGE_PATH } from '@/constants'
 
 const LandingPage = React.lazy(() => import('@/components/pages/Landing'))
+const NotFoundPage = React.lazy(() => import('@/components/pages/NotFound'))
 
 export default () => (
   <Router>
     <React.Suspense fallback={<Loader />}>
-      <Route exact path={LANDING_PAGE_PATH} component={LandingPage} />
-      <SecuredRoute
-        path="/success"
-        component={LandingPage}
-        strategy={successStrategyExample} />
-      <SecuredRoute
-        path="/failure"
-        component={LandingPage}
-        strategy={failureStrategyExample} />
+      <Switch>
+        <Route
+          exact
+          path={LANDING_PAGE_PATH}
+          component={LandingPage} />
+
+        <SecuredRoute
+          path="/success"
+          component={LandingPage}
+          strategy={successStrategyExample} />
+
+        <SecuredRoute
+          path="/failure"
+          component={LandingPage}
+          strategy={failureStrategyExample} />
+
+        <Route path="*" component={NotFoundPage} />
+      </Switch>
     </React.Suspense>
   </Router>
 )
