f1664f87286a9bbf440925f794ae8bd18b5dd171
Maksim Tarhunakau <maksimtarhunakau@MacBook-Pro-Maksim-2.local>
2019-09-26 22:57:47 +0300

    Adding Secured route and example strategies

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Loader from '@/components/blocks/global/Loader'
+import SecuredRoute from '@/components/wrappers/SecuredRoute'
+import {
+  successStrategyExample,
+  failureStrategyExample,
+} from '@/routerPermissions'
 
 import { LANDING_PAGE_PATH } from '@/constants'
 
@@ -11,6 +16,14 @@ export default () => (
   <Router>
     <React.Suspense fallback={<Loader />}>
       <Route exact path={LANDING_PAGE_PATH} component={LandingPage} />
+      <SecuredRoute
+        path="/success"
+        component={LandingPage}
+        strategy={successStrategyExample} />
+      <SecuredRoute
+        path="/failure"
+        component={LandingPage}
+        strategy={failureStrategyExample} />
     </React.Suspense>
   </Router>
 )
